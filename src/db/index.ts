8b91fbe9397913d0e505af4aa746130f44419fa8
Ameya Shrivastava <ameyshri051@gmail.com>
2025-07-10 20:30:41 +0530

    Add dotenv configuration to load environment variables for database connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const client = postgres(process.env.DATABASE_URL, {prepare: false,})
+export const client = postgres(process.env.DATABASE_URL)
 export const db = drizzle(client);
